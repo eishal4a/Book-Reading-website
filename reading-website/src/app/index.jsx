7bfc69ae0b856eeb7b432da14e698b13f5285b63
Eishal <eishal.cecos@gmail.com>
2025-03-16 19:17:40 +0500

    we
    we

---
+++ reading-website/src/app/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App'; // Assuming you have an App component in App.js
+
+ReactDOM.render(<App />, document.getElementById('root'));
